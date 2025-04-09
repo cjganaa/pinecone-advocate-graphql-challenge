d306982115dce4c4f456fae832bd9a179ff90ce9
cjganaa <ganaaeenee02@gmail.com>
2025-04-09 23:14:54 +0800

    todo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title></title>
+        <title>Todo</title>
       </Head>
       <body>
         <Main />
